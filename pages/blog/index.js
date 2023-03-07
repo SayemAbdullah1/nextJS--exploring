15a6faa06b7c83bded428393c26127c1599b61ba
SayemAbdullah1 <dxsayem0@gmail.com>
2023-03-07 16:36:09 +0600

    programmically routing

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,12 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const Blog = ({blogId = 200}) => {
+    const router = useRouter()
+    const handleSubmit = () =>{
+        router.replace("/about")
+    }
     return (
         <div>
             <Link href="blog/1">
@@ -13,6 +18,7 @@ const Blog = ({blogId = 200}) => {
             <Link href={`/blog/${blogId}`} replace>
                 <h1>blog 200</h1>
             </Link>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     );
 };
